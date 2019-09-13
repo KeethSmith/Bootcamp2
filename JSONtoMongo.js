ef6e73c398ed3f3cd7cb9fc91b3e2942a597838b
Keeth Smith <32115132+Spelunker30@users.noreply.github.com>
2019-09-13 03:11:38 -0400

    commit

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -7,11 +7,13 @@ var fs = require('fs'),
     mongoose = require('mongoose'), 
     Schema = mongoose.Schema, 
     Listing = require('./ListingSchema.js'), 
-    config = require('./config');
+    config = require('./config.js');
 
 /* Connect to your database using mongoose - remember to keep your key secret*/
 //see https://mongoosejs.com/docs/connections.html
 //See https://docs.atlas.mongodb.com/driver-connection/
+
+mongoose.connect(config.db.uri, {useNewUrlParser: true});
 
 /* 
   Instantiate a mongoose model for each listing object in the JSON file, 
@@ -22,7 +24,11 @@ var fs = require('fs'),
  */
 
 
-/*  
-  Check to see if it works: Once you've written + run the script, check out your MongoLab database to ensure that 
-  it saved everything correctly. 
- */
+
+fs.readFile('listings.json', 'utf8', function(err, data){
+  if(err) throw err
+
+  JSON.parse(data).entries.forEach(function(element) {
+    Listing.create(element);
+  });
+});
